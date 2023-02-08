707a2a06e94e2d8c80e6f5ee507d9def7ee80648
Louan Fontenele <louanbastos61@gmail.com>
2023-02-08 02:35:31 -0300

    Delete some tags and change the script

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const axios = require("axios");
-const json2xml = require("xml-js").json2xml;
-const express = require("express");
+import axios from "axios";
+import { json2xml } from "xml-js";
+import express from "express";
+import chalk from "chalk";
 
 const app = express();
 
 const PORT = 8080;
 
 app.get("/news", (request, response) => {
-  axios.get("https://launchercontent.mojang.com/news.json").then(result => {
-
-    let xml = json2xml(JSON.stringify(result.data),{compact: true});
+  axios.get("https://launchercontent.mojang.com/news.json").then((result) => {
+    let xml = json2xml(JSON.stringify(result.data), { compact: true });
 
     xml = xml.replaceAll("<entries>", "<item>");
     xml = xml.replaceAll("</entries>", "</item>");
-    xml = xml.replaceAll("/images/", "https://launchercontent.mojang.com/images/");
+    xml = xml.replaceAll(
+      "/images/",
+      "https://launchercontent.mojang.com/images/"
+    );
     xml = xml.replaceAll("<readMoreLink>", "<link>");
     xml = xml.replaceAll("</readMoreLink>", "</link>");
     xml = xml.replaceAll("<text>", "<description>");
     xml = xml.replaceAll("</text>", "</description>");
     xml = xml.replaceAll("<date>", "<pubDate>");
     xml = xml.replaceAll("</date>", "</pubDate>");
-    
-    response.set("Content-type","application/xml");
+    xml = xml.replaceAll("<newsPageImage>", "<image>");
+    xml = xml.replaceAll("</newsPageImage>", "</image>");
+    xml = xml.replaceAll("<dimensions>", "");
+    xml = xml.replaceAll("</dimensions>", "");
+
+    response.set("Content-type", "application/xml");
     response.type("application/xml");
     response.status(200).send(`
     <rss version="2.0">
@@ -30,10 +37,9 @@ app.get("/news", (request, response) => {
         <description>Minecraft Oficial RSS</description>
         ${xml}
       </channel>
-    </rss>`
-    );
-  })
-})
+    </rss>`);
+  });
+});
 
 app.listen(PORT);
-console.log(`Server online and running on port: ${PORT}`)
+console.log(chalk.green(`Server is online and running on port: ${PORT}`));
